110f0df7f7c1799067cbf9e3c15b296f0f68d359
Andrew Slavin <andrew.slavin@gmail.com>
2019-01-02 13:15:41 +0000

    Flesh out project with base classes and a simple test for the Google homepage

---
+++ framework/src/main/java/sbca/framework/core/BaseTest.java
@@ -0,0 +1,41 @@
+package sbca.framework.core;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeSuite
+    public void beforeSuite() {
+        //System.setProperty("headless", "false");
+        //String headless = System.getProperty("headless");
+
+        ChromeDriverManager.chromedriver();
+        driver = new ChromeDriver();
+//        if("true".equals(headless)) {
+//            ChromeOptions chromeOptions = new ChromeOptions();
+//            chromeOptions.addArguments("--headless");
+//            driver = new ChromeDriver(chromeOptions);
+//        } else {
+//            driver = new ChromeDriver();
+//        }
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        if(null != driver) {
+            driver.close();
+            driver.quit();
+        }
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
